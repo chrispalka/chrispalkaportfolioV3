d1d35b71aa6a0969c85d0f349f8e1d2e41293181
Chris Palka <cpalka87@gmail.com>
2026-03-25 21:40:34 -0400

    Add Prettier with tailwindcss plugin and begin Navbar implementation

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,15 +1,21 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ThemeContext } from "../../context/ThemeContext";
 
 import clsx from "clsx";
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
   const theme = useContext(ThemeContext);
   return (
-    <div>
+    <nav
+      className={clsx(
+        isOpen && "left-0 h-screen w-screen",
+        "absolute -left-100 border-2 border-red-500",
+      )}
+    >
       <h1 className={clsx(theme === "dark" && "bg-black text-white")}>
         Navbar
       </h1>
-    </div>
+    </nav>
   );
 }
